db98c413078564112e42a55ac5692a7f2cec095e
joohyosung <116233098+joohyosung@users.noreply.github.com>
2023-02-09 10:45:29 +0900

    Initial commit

---
+++ myCalc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def get_plus(a,b):
+    return a+b
+
+def get_minus(a,b):
+    return a-b
+
+def get_multiply(a,b):
+    return a*b
+
+def get_division(a,b):
+    if b!=0:
+        return a/b
+    else:
+        return 'zero division error'
+
